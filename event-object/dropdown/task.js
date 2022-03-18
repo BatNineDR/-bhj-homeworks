37692aae0fd2f3db948a721afa3407529c1f4e31
Alexandr <61879374+BatNineDR@users.noreply.github.com>
2022-03-18 20:07:11 +0300

    dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,19 @@
+let dropdownValue = document.querySelector('.dropdown__value');
+let dropdownList = document.querySelector('.dropdown__list');
+let dropdownItem = Array.from(document.querySelectorAll('.dropdown__item'));
+let dropdownLink = Array.from(document.querySelectorAll('.dropdown__link'));
+
+dropdownValue.addEventListener("click", openFunction);
+
+function openFunction() {
+    dropdownList.classList.toggle('dropdown__list_active');
+}
+
+for (let i = 0; i < dropdownItem.length; i++) {
+    dropdownItem[i].onclick = function() {
+        dropdownValue.textContent = this.textContent;
+        dropdownList.classList.remove('dropdown__list_active');
+        return false;
+
+    }
+}
