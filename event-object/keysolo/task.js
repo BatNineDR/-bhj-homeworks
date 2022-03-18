3f704279b7bcd40033dc835d14e722ab1767e22e
Alexandr <61879374+BatNineDR@users.noreply.github.com>
2022-03-18 22:53:44 +0300

    keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,15 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
-  }
+    document.addEventListener("keyup", (event) => {
+      let currentSymbol = this.currentSymbol.textContent;
+      if (currentSymbol == event.key) {
+          this.success();
+      } else {
+          this.fail();
+      }
+  });
+}
 
   success() {
     this.currentSymbol.classList.add('symbol_correct');
